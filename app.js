69f53f2ae7c49db6dc0d43ce77421797ae406d73
Matt Shirey <mattshirey@gmail.com>
2023-07-18 19:36:20 -0400

    rebuilding for only one team

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-const cors = require('cors')
+//const cors = require('cors')
 //const { MongoClient, ServerApiVersion } = require('mongodb')
 
 //
@@ -20,7 +20,7 @@ const app = express()
 
 app.use(bodyParser.json())
 
-app.use(cors())
+//app.use(cors())
 
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*')
