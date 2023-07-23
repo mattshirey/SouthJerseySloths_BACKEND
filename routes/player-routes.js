fdea3d03cadfa172d85f11534e8dda5a137df35a
Matt Shirey <mattshirey@gmail.com>
2023-07-23 13:54:39 -0400

    fix some things

--- routes/player-routes.js
+++ routes/player-routes.js
@@ -2,11 +2,7 @@ const express = require('express')
 const playerControllers = require('../controllers/player-controller')
 const router = express.Router()
 
-router.get(
-	'/:playerId/history',
-	fileUpload.single('image'),
-	playerControllers.getPlayerHistoryByPlayerId
-)
+router.get('/:playerId/history', playerControllers.getPlayerHistoryByPlayerId)
 //
 //
 router.post('/:playerId/photo', playerControllers.uploadPhoto)
