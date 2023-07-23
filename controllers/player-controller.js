d3cbe330f54e16009e6f94cfa4ce404849220e5e
Matt Shirey <mattshirey@gmail.com>
2023-07-23 15:31:26 -0400

    fix some things

--- controllers/player-controller.js
+++ controllers/player-controller.js
@@ -6,6 +6,7 @@ const Player = require('../models/player')
 const RosterPlayer = require('../models/rosterPlayer')
 const RosterPlayerStatsPerGame = require('../models/rosterPlayerStatsPerGame')
 const fileUpload = require('../middleware/file-upload')
+const Photo = require('../models/photo')
 
 const getPlayerHistoryByPlayerId = async (req, res, next) => {
 	console.log('inside getPlayerHistoryByPlayer')
@@ -36,6 +37,16 @@ const getPlayerHistoryByPlayerId = async (req, res, next) => {
 //
 const uploadPhoto = async (req, res, next) => {
 	console.log('inside uploadPhoto')
+	const playerId = req.params.playerId
+	const photo = req.file.filename
+	const newUserData = photo
+
+	const newPhoto = new Photo(newUserData)
+
+	newPhoto
+		.save()
+		.then(() => res.json('Photo added'))
+		.catch((err) => res.status(400).json('Error: ' + err))
 }
 //
 //
--- routes/player-routes.js
+++ routes/player-routes.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const playerControllers = require('../controllers/player-controller')
 const router = express.Router()
+const fileUpload = require('../middleware/file-upload')
 
 router.get('/:playerId/history', playerControllers.getPlayerHistoryByPlayerId)
 //
 //
-router.post('/:playerId/photo', playerControllers.uploadPhoto)
+router.post(
+	'/:playerId/photo',
+	fileUpload.single('photo'),
+	playerControllers.uploadPhoto
+)
 //
 //
 module.exports = router
