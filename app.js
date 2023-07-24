669212df60ef3d5b52d02020d2ca600662c0dde8
Matt Shirey <mattshirey@gmail.com>
2023-07-23 20:26:32 -0400

    fix some things

--- app.js
+++ app.js
@@ -44,7 +44,10 @@ app.get('/', (req, res) => {
 //
 //
 app.use((req, res, next) => {
-	const error = new HttpError('Could not find this route.', 404)
+	const error = new HttpError(
+		'Could not find this route.' + req + ' ' + res,
+		404
+	)
 	throw error
 })
 
