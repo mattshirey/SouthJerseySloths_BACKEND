6c386eff88ed0fb9b3dafd12feec800a3c3b0b7c
Matt Shirey <mattshirey@gmail.com>
2023-08-15 08:26:01 -0400

    edit registrant

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -34,7 +34,7 @@ const fileUpload = multer({
 	limits: 50000000,
 	storage: multer.diskStorage({
 		destination: (req, file, cb) => {
-			cb(null, (__dirname, '../uploads/images'))
+			cb(null, process.cwd() + '/uploads/images')
 		},
 		filename: (req, file, cb) => {
 			const ext = MIME_TYPE_MAP[file.mimetype]
