c9b2c773120d1d5262388bfc22eb35a9a1082a80
Matt Shirey <mattshirey@gmail.com>
2023-08-15 08:16:21 -0400

    edit registrant

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -1,6 +1,8 @@
 const multer = require('multer')
 const { v4: uuidv4 } = require('uuid')
 
+const path = require('path')
+
 const MIME_TYPE_MAP = {
 	'image/png': 'png',
 	'image/jpeg': 'jpeg',
@@ -32,7 +34,7 @@ const fileUpload = multer({
 	limits: 50000000,
 	storage: multer.diskStorage({
 		destination: (req, file, cb) => {
-			cb(null, __dirname)
+			cb(null, path.join(__dirname, '/uploads/images'))
 		},
 		filename: (req, file, cb) => {
 			const ext = MIME_TYPE_MAP[file.mimetype]
