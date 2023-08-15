893942b593bfb894e9533917614548e4e5863e4f
Matt Shirey <mattshirey@gmail.com>
2023-08-15 18:20:53 -0400

    image upload

--- controllers/player-controller.js
+++ controllers/player-controller.js
@@ -37,6 +37,7 @@ const getPlayerHistoryByPlayerId = async (req, res, next) => {
 //
 const uploadPhoto = async (req, res, next) => {
 	console.log('inside uploadPhoto: ' + req.file)
+	console.dir(req.file)
 	//const playerId = req.params.playerId
 	const photo = req.file.path
 	//const newUserData = photo
