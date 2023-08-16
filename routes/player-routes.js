cc2b9f018c351d1c0c3d33de76a5e763266b731a
Matt Shirey <mattshirey@gmail.com>
2023-08-15 21:07:07 -0400

    image upload

--- routes/player-routes.js
+++ routes/player-routes.js
@@ -9,7 +9,7 @@ router.get('/:playerId/history', playerControllers.getPlayerHistoryByPlayerId)
 console.log('inside player-routes...')
 router.post(
 	'/:rosterPlayerId/photo',
-	fileUpload.single('file'),
+	fileUpload.single('image'),
 	playerControllers.uploadPhoto
 )
 //
